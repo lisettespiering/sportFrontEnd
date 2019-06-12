2668076aa423df2290930afcfb46f54e856ebfb4
BasBosman <bas_bosman1@hotmail.com>
2019-06-12 14:23:08 +0200

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { menuComponent } from './menu/menu.component';
+
+import { HomeComponent } from './home/home.component';
+import { LocatiesComponent } from './locaties/locaties.component';
+import { AccountComponent } from './account/account.component';
+import { OverOnsComponent } from './overOns/overOns.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    menuComponent,
+    HomeComponent,
+    LocatiesComponent,
+    AccountComponent,
+    OverOnsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/overOns/overOns.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overOns',
+  templateUrl: './overOns.component.html',
+  styleUrls: ['./overOns.component.css']
+})
+export class OverOnsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
