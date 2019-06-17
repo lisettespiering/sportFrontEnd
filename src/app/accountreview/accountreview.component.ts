7e0ce59c60c4e67d8399ca780acca0c12eb986a5
BasBosman <bas_bosman1@hotmail.com>
2019-06-17 15:14:47 +0200

    wijzigingen

---
+++ src/app/accountreview/accountreview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accountreview',
+  templateUrl: './accountreview.component.html',
+  styleUrls: ['./accountreview.component.css']
+})
+export class AccountreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
